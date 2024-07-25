b7c825d42fd246d93cbafc515c2c3285d2abf94b
Dominik Picheta <dominik@cloudflare.com>
2024-07-25 18:21:57 +0000

    Generate package memory snapshot for validator.

--- src/pyodide/types/artifacts.d.ts
+++ src/pyodide/types/artifacts.d.ts
@@ -1,15 +1,20 @@
 declare namespace ArtifactBundler {
+  type MemorySnapshotResult = {
+    snapshot: Uint8Array;
+    importedModulesList: Array<string>;
+  }
+
   const hasMemorySnapshot: () => boolean;
   const isEwValidating: () => boolean;
   const isEnabled: () => boolean;
-  const uploadMemorySnapshot: (toUpload: Uint8Array) => Promise<boolean>;
+  const uploadMemorySnapshot: (toUpload: MemorySnapshotResult) => Promise<boolean>;
   const readMemorySnapshot: (
     offset: number,
     buf: Uint32Array | Uint8Array,
   ) => void;
   const getMemorySnapshotSize: () => number;
   const disposeMemorySnapshot: () => void;
-  const storeMemorySnapshot: (snap: Uint8Array) => void;
+  const storeMemorySnapshot: (snap: MemorySnapshotResult) => void;
 }
 
 export default ArtifactBundler;
